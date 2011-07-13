c419558d4a75576c642bd1a87b51697f4ad9a64c
joaopalotti <joaopalotti@gmail.com>
2011-07-13 16:14:26 -0300

    Fitness test

---
+++ testesFitness/NB-macro/acm/fold3/run.sh
@@ -0,0 +1,18 @@
+fold=$(pwd | sed 's/fold/ /g' | cut -f2 -d' ')
+
+base=$(pwd | sed 's/\/fold/ /g' | cut -f 1 -d' ' | sed 's/\NB-macro\// /g' | cut -f 2 -d' ')
+echo $base
+echo $fold
+
+path=../../../../arquivos/$base
+
+treinopath=$path/CV5/treino/treino$fold
+validacaopath=$path/CV5/validacao/validacao$fold
+testepath=$path/CV5/teste/teste$fold
+brunoro=fold$fold.gpvis
+
+macro="-macroNotMicro"
+
+python runme.py ./gp-cred -treino $treinopath -validacao $validacaopath -teste $testepath -brunoro $brunoro -dontsave $macro
+
+
