6fff3011f515ca17ea122cdc3c348033bc54aff2
Joao Rafael de Moura Palotti <palotti@hydra1.speed.dcc.ufmg.br>
2011-07-14 11:46:05 -0300

    hier4-k1

---
+++ testesEnzymas/hier4-k1/fold0/run.sh
@@ -0,0 +1,14 @@
+fold=$(pwd | sed 's/fold/ /g' | cut -f2 -d' ')
+
+path=../../../fonte/enzymes/files/hier4/
+
+treinopath=$path/fold$fold/_treino$fold
+validacaopath=$path/fold$fold/_validacao$fold
+testepath=$path/fold$fold/_teste$fold
+brunoro=fold$fold.gpvis
+
+graph=../../../fonte/enzymes/top950.blast.ok3.sorted
+
+python runme.py ./gp-cred -treino $treinopath -validacao $validacaopath -teste $testepath -brunoro $brunoro -dontsave -num 15 -noterms -knn 1 -KNNOptimize -g 1 $graph
+
+
