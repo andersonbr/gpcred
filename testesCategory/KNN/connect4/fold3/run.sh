e9864df79afdbbf3a6d37030f2b016c4d1d9956c
Joao Rafael de Moura Palotti <palotti@hydra6.hydra.speed.dcc.ufmg.br>
2011-07-15 21:16:21 -0300

    files

--- testesCategory/KNN/connect4/fold3/run.sh
+++ testesCategory/KNN/connect4/fold3/run.sh
@@ -3,13 +3,13 @@ fold=$(pwd | sed 's/fold/ /g' | cut -f2 -d' ')
 colecao=$(pwd | sed 's/\/fold/ /g' | cut -f 1 -d' '| sed 's/\/KNN\// /g' | cut -f 2 -d' ')
 
 base=../../../../colecoes/uci/cat/$colecao/fold$fold/
-
+k=10
 treinopath=$base/_treino$fold.jformat
 validacaopath=$base/_validacao$fold.jformat
 testepath=$base/_teste$fold.jformat
 brunoro=fold$fold.gpvis
 
 
-python runme.py ./gp-cred -treino $treinopath -validacao $validacaopath -teste $testepath -brunoro $brunoro -dontsave -cat $cat
+python runme.py ./gp-cred -treino $treinopath -validacao $validacaopath -teste $testepath -brunoro $brunoro -dontsave -cat $cat -knn $k
 
 
