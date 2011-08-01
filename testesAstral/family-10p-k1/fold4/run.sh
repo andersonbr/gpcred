d5e2c4ffd4f86b07f0b20377c093528766bbff4f
Joao Rafael de Moura Palotti <palotti@hydra4.hydra.speed.dcc.ufmg.br>
2011-08-01 13:37:36 -0300

    results

--- testesAstral/family-10p-k1/fold4/run.sh
+++ testesAstral/family-10p-k1/fold4/run.sh
@@ -12,6 +12,5 @@ graph=$base/outfile.astral.blast.ok4
 
 python runme.py ./gp-cred -treino $treinopath -validacao $validacaopath -teste $testepath -brunoro $brunoro -knn $k -num 15 -g 1 $graph
 
-
 #echo "python runme.py ./gp-cred -treino $treinopath -validacao $validacaopath -teste $testepath -brunoro $brunoro -knn $k -num 15 -g 1 $graph"
 
