093ef43658eee10a1187800e99a818aa03534a7f
Joao Rafael de Moura Palotti <palotti@hydra5.hydra.speed.dcc.ufmg.br>
2011-08-03 17:09:36 -0300

    minor chaanges

--- testesEnzymas/hier4-k1-50p/fold0/run.sh
+++ testesEnzymas/hier4-k1-50p/fold0/run.sh
@@ -13,6 +13,6 @@ brunoro=fold$fold.gpvis
 
 grafopath=$path/../../top950.blast.ok3.sorted
 
-python runme.py ./gp-cred -treino $treinopath -validacao $validacaopath -teste $testepath -brunoro $brunoro -noterms -g 1 $grafopath -num 15 -knn 1 -knnoptimize -load pop.save
+python runme.py ./gp-cred -treino $treinopath -validacao $validacaopath -teste $testepath -brunoro $brunoro -noterms -g 1 $grafopath -num 15 -knn 1 -knnoptimize
 
 
