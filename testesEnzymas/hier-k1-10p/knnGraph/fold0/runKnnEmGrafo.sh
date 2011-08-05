89945d960b7fa6c5b9c1205940034067b35cd081
joaopalotti <joaopalotti@gmail.com>
2011-08-05 15:13:14 -0300

    knn graph

---
+++ testesEnzymas/hier-k1-10p/knnGraph/fold0/runKnnEmGrafo.sh
@@ -0,0 +1,14 @@
+fold=$(pwd | sed 's/fold/ /g' | cut -f2 -d' ')
+
+path=../../../../fonte/enzymes/files/hier4_10p/
+
+treinopath=$path/fold$fold/_treino$fold
+validacaopath=$path/fold$fold/_validacao$fold
+testepath=$path/fold$fold/_teste$fold
+brunoro=fold$fold.gpvis
+
+grafopath=$path/../../top950.blast.ok3.sorted
+
+python knnEmGrafo.py $treinopath $validacaopath $grafopath $testepath
+
+
