78b8c9fe7d80f26a6d09074fe7712b6f20aecb7f
Joao Rafael de Moura Palotti <palotti@hydra2.speed.dcc.ufmg.br>
2011-08-19 12:51:57 -0300

    yeah baby

---
+++ testesAstral/family-10p-k1All/knnGraphWeight/fold2/runKnnEmGrafo.sh
@@ -0,0 +1,14 @@
+fold=$(pwd | sed 's/fold/ /g' | cut -f2 -d' ')
+
+path=../../../../fonte/enzymes/astral/family10pAll/
+
+treinopath=$path/treino/_treino$fold
+validacaopath=$path/validacao/_validacao$fold
+testepath=$path/teste/_teste$fold
+brunoro=fold$fold.gpvis
+
+grafopath=$path/outfile.astral.blast.ok4 
+
+python knnEmGrafo.py $treinopath $validacaopath $grafopath $testepath
+
+
