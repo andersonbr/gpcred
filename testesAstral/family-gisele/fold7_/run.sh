b031bfad055e86f2485f25eb0e1f9107894796f1
joaopalotti <joaopalotti@gmail.com>
2011-09-01 05:56:55 -0300

    go go go!!!

---
+++ testesAstral/family-gisele/fold7_/run.sh
@@ -0,0 +1,16 @@
+fold=$(pwd | sed 's/fold/ /g' | cut -f2 -d' ')
+
+base=../../../fonte/enzymes/astral/family10f
+treinopath=$base/fold$fold/_treino
+validacaopath=$base/fold$fold/_validacao
+testepath=$base/fold$fold/_teste
+
+k=1
+brunoro=fold$fold.gpvis
+
+graph=$base/outfile.astral.blast.ok4
+
+python runme.py ./gp-cred -noterms -treino $treinopath -validacao $validacaopath -teste $testepath -brunoro $brunoro -knn $k -num 15 -g 1 $graph -eval eval.in eval.out
+
+#echo "python runme.py ./gp-cred -treino $treinopath -validacao $validacaopath -teste $testepath -brunoro $brunoro -knn $k -num 15 -g 1 $graph"
+
